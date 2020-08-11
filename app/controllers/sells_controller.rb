1ead92643acec0dcb76cc0451573de40927e158a
neibis-zrm <berun0d02a46@gmail.com>
2020-08-11 18:18:52 +0900

    [WIP] add function image upload

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -2,6 +2,7 @@ class SellsController < ApplicationController
 
   def index
     @item = Item.new()
+    @image = ItemImage.new()
   end
 
   def create
@@ -10,7 +11,8 @@ class SellsController < ApplicationController
     #フォームの取得(カテゴリ、phase:出品中 追加)
     @item = Item.new(params_int(item_params).merge(category: category_data, phase_id: 1))
     binding.pry
-    if @item.save
+    @image = @item.item_images.new(item_image_params)
+    if @item.save && @image.save
       respond_to do |format|
         #出品完了(出品完了ページに飛ばす事)
         format.html {redirect_to sells_path, notice: '出品完了'}
@@ -21,10 +23,15 @@ class SellsController < ApplicationController
   end
 
   private
-  
-  #ストロングパラメータ
+
+  #ストロングパラメータ(items)
   def item_params
     params.require(:item).permit(:name, :item_detail, :status_id, :delivery_days, :delivery_to_pay_id, :price)
+  end
+
+  #ストロングパラメータ(item_images)
+  def item_image_params
+    params.require(:item).require(:item_image).permit(:image)
   end
 
   #数値に変換可能な文字列を数値に変換する
