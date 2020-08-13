f136a2202ba15f6bb3e6aed3fcb26b189d1451ec
neibis-zrm <berun0d02a46@gmail.com>
2020-08-13 11:42:25 +0900

    add function image upload(multiple unit)

--- app/controllers/sells_controller.rb
+++ app/controllers/sells_controller.rb
@@ -2,7 +2,7 @@ class SellsController < ApplicationController
 
   def index
     @item = Item.new()
-    @image = ItemImage.new()
+    @item.item_images.new()
   end
 
   def create
@@ -10,8 +10,7 @@ class SellsController < ApplicationController
     category_data = Category.find_by(id: params[:item][:category])
     #フォームの取得(カテゴリ、phase:出品中 追加)
     @item = Item.new(params_int(item_params).merge(category: category_data, phase_id: 1))
-    @image = @item.item_images.new(item_image_params)
-    if @item.save && @image.save
+    if @item.save
       respond_to do |format|
         #出品完了(出品完了ページに飛ばす事)
         format.html {redirect_to sells_path, notice: '出品完了'}
@@ -25,13 +24,8 @@ class SellsController < ApplicationController
 
   #ストロングパラメータ(items)
   def item_params
-    params.require(:item).permit(:name, :item_detail, :status_id, :delivery_days, :delivery_to_pay_id, :price)
+    params.require(:item).permit(:name, :item_detail, :status_id, :delivery_days, :delivery_to_pay_id, :price,item_images_attributes: [:image])
     # params.require(:item).permit(:name, :item_detail, :status_id, :delivery_days, :delivery_to_pay_id, :price).merge(sell_user_id: current_user.id)
-  end
-
-  #ストロングパラメータ(item_images)
-  def item_image_params
-    params.require(:item).require(:item_image).permit(:image)
   end
 
   #数値に変換可能な文字列を数値に変換する
@@ -45,8 +39,12 @@ class SellsController < ApplicationController
   #paramsの内容を数値に変換
   def params_int(model_params)
     model_params.each do |key,value|
-      if integer_string?(value)
-        model_params[key]=value.to_i
+      begin
+        if integer_string?(value)
+          model_params[key]=value.to_i
+        end              
+      rescue => exception
+        # nothing
       end
     end
     return model_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  has_many :item_images, dependent: :destroy
+  has_many :item_images
+  accepts_nested_attributes_for :item_images, allow_destroy: true
   has_many :messages
   # belongs_to :sell_user, class_name: "User"
   # belongs_to :buy_user, class_name: "User", optional: true
