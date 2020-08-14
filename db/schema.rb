216a92a02472801f0222004cd522c514292e932a
neibis-zrm <berun0d02a46@gmail.com>
2020-08-14 16:01:13 +0900

    adjust for pullrequest

--- db/schema.rb
+++ db/schema.rb
@@ -10,59 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_053842) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
-  end
-
-  create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "item_id", null: false
-    t.string "image", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_item_images_on_item_id"
-  end
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "category_id", null: false
-    t.integer "phase_id", null: false
-    t.integer "status_id", null: false
-    t.integer "delivery_to_pay_id", null: false
-    t.string "name", null: false
-    t.integer "price", null: false
-    t.text "item_detail"
-    t.integer "delivery_days", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_items_on_category_id"
-    t.index ["name"], name: "index_items_on_name"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "street_id", null: false
-    t.string "image"
-    t.string "surname", null: false
-    t.string "surname_kana", null: false
-    t.string "firstname", null: false
-    t.string "firstname_kana", null: false
-    t.string "nick_name", null: false
-    t.string "Birthday", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "item_images", "items"
-  add_foreign_key "items", "categories"
 end
