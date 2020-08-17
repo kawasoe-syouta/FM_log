c17f91dc2a78f7980befffc448e9335f90f0f15a
neibis-zrm <berun0d02a46@gmail.com>
2020-08-17 12:36:47 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'items#index'
+  resources :sells, only: [:index,:create]
+  resources :items, only: [:index,:show]
   resources :purchases, only: :index
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
