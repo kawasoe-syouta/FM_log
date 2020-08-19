6a213da0b12902df069b7985f871fa4221e1fec8
Jumpei-Tanikawa <tjinppsi0815@gmail.com>
2020-08-19 14:11:39 +0900

    sign_up-function

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :sells, only: [:index,:create]
   resources :items, only: [:index,:show]
+  resources :users, only: [:new,:create]
 end
