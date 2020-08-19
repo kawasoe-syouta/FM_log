357962562528a0b7d9d6bac4a503d7c6dee32599
neibis-zrm <berun0d02a46@gmail.com>
2020-08-19 12:02:51 +0900

    [WIP] credit card info register

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,4 +2,17 @@ class PurchasesController < ApplicationController
   def index
     @purchases = Purchase.all
   end
+
+  def update
+    @item = Item.find(params[:item_id])
+    if @item.update(phase_id: 2)
+      render :index
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(phase_id: 2)
+  end
+
 end
