7a3fb291b557b5eed4c4d545f860a8a51f07d478
tomoki-ito-annyon <s.m_soccer14@ezweb.ne.jp>
2020-08-20 16:25:40 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,18 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :destroy]
   def index
+    items = Item.all
   end
   
   def show
-    
+  end
+
+  def destroy
+    item.destroy
+  end
+  
+  private
+  def set_item
+    @items = Item.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :purchases, only: :index
+  devise_for :users
+  resources :items, only: [:index, :show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
