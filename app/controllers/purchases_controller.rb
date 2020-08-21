1e30799e44ccb6b991e4e861a660279bc81e6d9e
neibis-zrm <berun0d02a46@gmail.com>
2020-08-21 21:04:47 +0900

    add function item buy for credit

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,18 +1,37 @@
 class PurchasesController < ApplicationController
-  def index
+  def show
     @purchases = Purchase.all
+    @carddata = card_index()
+    @item = Item.find(params[:item_id])
   end
 
   def update
+    Payjp.api_key = Rails.application.credentials.payjp[:PAYJP_PRIVATE_KEY]
     @item = Item.find(params[:item_id])
-    if @item.update(phase_id: 2)
-      render :index
+
+    # 出品中以外は購入不可
+    if @item.phase_id != 1
+      binding.pry()
+      redirect_to item_path(@item.id), alert: "購入済みです"
+    else
+      @item.with_lock do
+        @card = Credit.find_by(id: card_params[:card_id])
+        charge = Payjp::Charge.create(
+          amount: @item.price,
+          customer: Payjp::Customer.retrieve(@card.card_customer),
+          currency: 'jpy'
+        )
+        # 発送待ちに変更
+        @item.update(phase_id: 3)
+      end
     end
+
   end
 
   private
-  def item_params
-    params.require(:item).permit(phase_id: 2)
+
+  def card_params
+    params.require(:item).permit(:card_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   resources :sells, only: [:index,:create]
-  resources :items, only: [:index,:show]
+  resources :items, only: [:index,:show] do
+    resource :purchases, only: [:show, :update]    
+  end
   resources :users, only: [:new,:create]
-  resources :purchases, only: :index
   resources :credits, only: [:index, :show, :new, :create, :destroy]
 end
