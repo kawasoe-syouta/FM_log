227e6eeb9f8ca0acb93d790e0602111c82a1f5d7
neibis-zrm <berun0d02a46@gmail.com>
2020-08-24 02:45:15 +0900

    add register users

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] = {user: @user.attributes}
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nick_name,:email,:password,:firstname,:surname,:firstname_kana,:surname_kana,:Birthday)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get 'streets', to: 'users/registrations#new_streets'
+    post 'streets', to: 'users/registrations#create_streets'
+  end
   root 'items#index'
   resources :sells, only: [:index,:create]
   resources :items, only: [:index,:show]
-  resources :users, only: [:new,:create]
   resources :purchases, only: :index
   resources :credits, only: [:index, :show, :new, :create, :destroy]
 end
