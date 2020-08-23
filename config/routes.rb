bb914d7f8c837f7ee0781cba4ceebe31de3e799c
neibis-zrm <berun0d02a46@gmail.com>
2020-08-24 02:45:15 +0900

    add register users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   root to: 'items#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  devise_scope :user do
+    get 'streets', to: 'users/registrations#new_streets'
+    post 'streets', to: 'users/registrations#create_streets'
+  end
   resources :sells, only: [:index,:create]
   resources :items, only: [:index,:show, :destroy] do
     resource :purchases, only: [:show, :update]    
   end
-  resources :users, only: [:new,:create]
   resources :credits, only: [:index, :show, :new, :create, :destroy]
 end
