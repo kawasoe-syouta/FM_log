a534a2fc10240f0ff57a299d386bdcda2bdf842e
neibis-zrm <berun0d02a46@gmail.com>
2020-08-25 11:14:38 +0900

    [wip] buy items with users

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,17 @@
 class PurchasesController < ApplicationController
+
+  before_action :redirect_no_user
+
   def show
-    @purchases = Purchase.all
     @carddata = card_index()
     @item = Item.find(params[:item_id])
+    # 出品者が購入は禁止
+    if @item.sell_user_id == current_user.id 
+      redirect_to item_path(@item.id), alert: "購入することが出来ません"
+    end
   end
 
   def update
-
     # カードが選択されていない場合
     if card_params == nil
       return create_action_error("支払い方法を選択してください")
@@ -22,6 +27,7 @@ class PurchasesController < ApplicationController
       @item.with_lock do
         @card = Credit.find_by(id: card_params[:card_id])
 
+        binding.pry()
         # 支払い処理
         begin
           charge = Payjp::Charge.create(
@@ -34,7 +40,7 @@ class PurchasesController < ApplicationController
         end
 
         # 発送待ちに変更
-        @item.update(phase_id: 3)
+        @item.update(phase_id: 3, buy_user_id: current_user.id)
       end
     end
 
@@ -51,10 +57,13 @@ class PurchasesController < ApplicationController
   end
 
   def create_action_error(alert)
-    @purchases = Purchase.all
     @carddata = card_index()
     @item = Item.find(params[:item_id])
     return render :show, alert: alert
+  end
+
+  def redirect_no_user
+    redirect_to root_path unless user_signed_in?
   end
 
 end
