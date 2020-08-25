1257dddfc4b24e1b3429fb55d15035f52898af37
tomoki-ito-annyon <s.m_soccer14@ezweb.ne.jp>
2020-08-25 21:42:48 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :items, only: [:show, :destroy, :show_image]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all
     @parents = Category.where(ancestry: nil)
@@ -42,4 +44,9 @@ class ItemsController < ApplicationController
     @items = Item.find(params[:id])
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
