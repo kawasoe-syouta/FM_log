5211acc832fe4459e6686acb6717684880b0daed
tomoki-ito-annyon <s.m_soccer14@ezweb.ne.jp>
2020-08-25 21:42:48 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :items, only: [:show, :destroy]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
     @items = Item.all
   end
@@ -19,5 +21,11 @@ class ItemsController < ApplicationController
   private
   def items
     @items = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
