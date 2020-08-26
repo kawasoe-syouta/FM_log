f7d0fb4a632ddbecebe8f739b9277e219e4fffb0
kawasoe-syouta <d6hyj3dyr@gmail.com>
2020-08-26 11:45:21 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -31,40 +31,4 @@ Rails.application.routes.draw do
       get 'grandchild'
     end
   end
-<<<<<<< HEAD
-  resources :items do
-    collection do
-      get :search
-    end
-  end
-
-  resources :categories, only: [:index] do
-    member do
-      get 'parent'
-      get 'child'
-      get 'grandchild'
-    end
-=======
-
-  devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
-  }
-  devise_scope :user do
-    get 'streets', to: 'users/registrations#new_streets'
-    post 'streets', to: 'users/registrations#create_streets'
-  end
-  resources :items, only: [:index,:show,:destroy,:edit] do
-    resource :purchases, only: [:show, :update]    
->>>>>>> a8d73ed... commit
-  end
-
-  resources :categories, only: [:index] do
-    member do
-      get 'parent'
-      get 'child'
-      get 'grandchild'
-    end
-  end
-
 end
