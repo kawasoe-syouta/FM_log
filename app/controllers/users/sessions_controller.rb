e08499fd69327aa6a284722ea704262f78eec9a8
neibis-zrm <berun0d02a46@gmail.com>
2020-08-28 14:17:30 +0900

    fix session

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,7 +6,7 @@ class Users::SessionsController < Devise::SessionsController
   # GET /resource/sign_in
   def new
     @parents = Category.where(ancestry: nil)
-    # super
+    super
   end
 
   # POST /resource/sign_in
